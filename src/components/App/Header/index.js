046530508bafe799c821db3c3c295e893979cf2d
rednightingale <paomas@hotmail.fr>
2023-02-09 16:37:34 +0100

    Header: ajout du css

--- src/components/App/Header/index.js
+++ src/components/App/Header/index.js
@@ -1,10 +1,17 @@
 // -- Mes imports locaux
+import logo from '../../../assets/images/avatar-logo.svg';
 import "./style.scss";
 
 // -- Mon composant
 function Header() {
   return (
-    <div className="Header">Header</div>
+    <div className="Header">
+      <img
+        className="Header-logo"
+        src={logo}
+        alt="Avatar the Last Airbender logo"
+      />
+    </div>
   );
 }
 
