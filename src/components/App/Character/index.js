fd1a2db86aa48e6a1e0f49aba599ad4329eabb84
rednightingale <paomas@hotmail.fr>
2023-05-15 17:47:07 +0200

    ResultsItem: quick fix on API calls

--- src/components/App/Character/index.js
+++ src/components/App/Character/index.js
@@ -33,64 +33,91 @@ function Character() {
     fetchData();
   }, []);
 
+  // Je n'arrive pas à déconstruire mon state "character" => pourquoi ???
+  // Attention !!!!!!
+  // Je dois toujours vérifier si mes objets sont dans des tableaux ou des strings
+  // Si se sont des tableaux, je peux mapper dessus
+  // Si se sont des strings, je prend la string telle quelle
+  const alternativeNames = Array.isArray(character.bio?.alternativeNames) ? character.bio?.alternativeNames.join(", ") : character.bio?.alternativeNames;
+  const position = Array.isArray(character.politicalInformation?.position) ? character.politicalInformation?.position.join(", ") : character.politicalInformation?.position;
+  const nationality = Array.isArray(character.bio?.nationality) ? character.bio?.nationality.join(", ") : character.bio?.nationality;
+  const ethnicity = Array.isArray(character.bio?.ethnicity) ? character.bio?.ethnicity.join(", ") : character.bio?.ethnicity;
+
   return (
     <article className="Character">
-      <header className="Character-header">
-        <h1 className="Character-header-title">NOM DU PERSO</h1>
-        <figure className="Character-header-figure">
-          <img
-            className="Character-header-figure-picture"
-            src={id === 8 ? `/img/Characters/Suki.jpg` : id === 11 ? `/img/Characters/Azula.jpg` : `/img/Characters/${name}.jpg`}
-            alt={`Character of ${name}`}
-          />
-          <figcaption className="Character-header-figure-caption">UNE FIGCAPTION</figcaption>
-        </figure>
-      </header>
+      <div className="Character-header-container">
+        <header className="Character-header">
 
-      <aside className="Character-aside">
-        <h2 className="Character-aside-title">Nation</h2>
-        <figure className="Character-aside-nation">
-          <img
-            className="Character-aside-nation-picture"
-            // src={}
-            alt={`Flag of ${name}'s nation`}
-          />
-        </figure>
-        <h2 className="Character-aside-title">In love with</h2>
-        <figure className="Character-aside-love">
-          <img
-            className="Character-aside-love-picture"
-            // src={}
-            alt={`${name}'s love interest`}
-          />
-        </figure>
-      </aside>
+          <h1 className="Character-header-title">{name}</h1>
 
-      <article className="Character-info">
-        <header className="Character-info-title">Character sheet</header>
-        <h3 className="Character-info-subtitle">alternativeNames</h3>
-        <p className="Character-info-text">alternativeNames</p>
-        <h3 className="Character-info-subtitle">profession</h3>
-        <p className="Character-info-text">profession</p>
-        <h3 className="Character-info-subtitle">nationality</h3>
-        <p className="Character-info-text">nationality</p>
-        <h3 className="Character-info-subtitle">ethnicity</h3>
-        <p className="Character-info-text">ethnicity</p>
-        <h3 className="Character-info-subtitle">ages</h3>
-        <p className="Character-info-text">ages</p>
-        <h3 className="Character-info-subtitle">weaponsOfChoice</h3>
-        <p className="Character-info-text">weaponsOfChoice</p>
-        <h3 className="Character-info-subtitle">fightingStyles</h3>
-        <p className="Character-info-text">fightingStyles</p>
-      </article>
+          <article className="Character-header-info">
+            {/* <header className="Character-header-info-title">Character sheet</header> */}
+            <h3 className="Character-header-info-subtitle">Alternative names</h3>
+            <p className="Character-header-info-text">{alternativeNames}</p>
+            <h3 className="Character-header-info-subtitle">Position</h3>
+            <p className="Character-header-info-text">{position}</p>
+            <h3 className="Character-header-info-subtitle">Nationality</h3>
+            <p className="Character-header-info-text">{nationality}</p>
+            <h3 className="Character-header-info-subtitle">Ethnicity</h3>
+            <p className="Character-header-info-text">{ethnicity}</p>
+            <h3 className="Character-header-info-subtitle">Age</h3>
+            {Array.isArray(character.bio?.ages) ?
+              character.bio?.ages.map((age) =>
+                <p className="Character-header-info-text" key={age}>{age.replace(`[3]`, "").replace(`[4]`, "").replace(`[5]`, "").replace(`[6]`, "")}</p>
+              ) : <p className="Character-header-info-text">{character.bio?.ages}</p>}
+            <h3 className="Character-header-info-subtitle">Weapons of choice</h3>
+            {Array.isArray(character.personalInformation?.weaponsOfChoice)
+              ? character.personalInformation?.weaponsOfChoice.map((weapon) => (
+                <p className="Character-header-info-text" key={weapon}>{weapon}</p>
+              ))
+              : <p className="Character-header-info-text">{character.personalInformation?.weaponsOfChoice}</p>}
+            <h3 className="Character-header-info-subtitle">Fighting styles</h3>
+            {Array.isArray(character.personalInformation?.fightingStyles)
+              ? character.personalInformation?.fightingStyles.map((item) => (
+                <p className="Character-header-info-text" key={item}>{item}</p>
+              ))
+              : <p className="Character-header-info-text">{character.personalInformation?.fightingStyles}</p>}
+          </article>
 
-      <section className="Character-allies">
-        <h2 className="Character-allies-title">allies</h2>
-      </section>
+        </header>
 
-      <section className="Character-ennemies">
-        <h2 className="Character-ennemies-title">enemies</h2>
-      </section>
+        <aside className="Character-header-aside">
+          <figure className="Character-header-aside-figure">
+            <img
+              className="Character-header-aside-figure-image"
+              src={id === 8 ? `/img/Characters/Suki.jpg` : id === 11 ? `/img/Characters/Azula.jpg` : `/img/Characters/${name}.jpg`}
+              alt={`Character of ${name}`}
+            />
+            {/* <figcaption className="Character-figure-caption"></figcaption> */}
+          </figure>
+          <h2 className="Character-header-aside-title">Nation</h2>
+          <figure className="Character-header-aside-nation">
+            <img
+              className="Character-header-aside-nation-image"
+              // src={}
+              alt={`Flag of ${name}'s nation`}
+            />
+          </figure>
+          <h2 className="Character-header-aside-title">In love with</h2>
+          <figure className="Character-header-aside-love">
+            <img
+              className="Character-header-aside-love-image"
+              // src={}
+              alt={`${name}'s love interest`}
+            />
+          </figure>
+        </aside>
+      </div>
+
+      <div className="Character-carousel-container">
+        <section className="Character-carousel-allies">
+          <h2 className="Character-carousel-allies-title">allies</h2>
+        </section>
+
+        <section className="Character-carousel-ennemies">
+          <h2 className="Character-carousel-ennemies-title">enemies</h2>
+        </section>
+      </div>
     </article>
   );
 }
--- src/components/App/Results/ResultsItem/index.js
+++ src/components/App/Results/ResultsItem/index.js
@@ -9,7 +9,9 @@ import { FaPlusCircle } from "react-icons/fa";
 import "../style.scss";
 
 // -- Mon composant
-function ResultsItem({ id, name, bio, chronologicalInformation, politicalInformation }) {
+function ResultsItem({
+  id, name, bio, chronologicalInformation, politicalInformation,
+}) {
   // Je récupère les différentes "position" des personnages à travers "politicalInformation"
   // Je ne garde que deux "positions" pour les cartes de présentation de personnage
   const [position, getPosition] = useState([]);
@@ -58,14 +60,14 @@ function ResultsItem({ id, name, bio, chronologicalInformation, politicalInforma
               <img
                 className="ResultsItem-flip-card-front-image-logo"
                 src={nationality === "Fire Nation" ? `img/Logo/logo-${nationality}.png` : nationality === "Air Nomad" ? `img/Logo/logo-${nationality}.png` : nationality === "Earth Kingdom" ? `img/Logo/logo-${nationality}.png` : `img/Logo/logo-${ethnicity}.png`}
-                alt={`Nation logo`}
+                alt="Nation logo"
               />
             </div>
             <div className="ResultsItem-flip-card-front-header-container">
               <header className="ResultsItem-flip-card-front-header">
                 <hr className="ResultsItem-flip-card-front-header-breakline" />
                 <h1 className="ResultsItem-flip-card-front-header-name">{id === 7 ? "Zuko" : id === 8 ? "Suki" : id === 11 ? "Azula" : name}</h1>
-                <h2 className="ResultsItem-flip-card-front-header-nationality">{nationality.length > 33 ? nationality.slice(0, 33) : nationality}</h2>
+                <h2 className="ResultsItem-flip-card-front-header-nationality">{nationality?.length > 33 ? nationality?.slice(0, 33) : nationality}</h2>
                 {position.map((job) => (
                   <p className="ResultsItem-flip-card-front-header-position" key={job}>{job}</p>
                 ))}
@@ -83,17 +85,17 @@ function ResultsItem({ id, name, bio, chronologicalInformation, politicalInforma
               <img
                 className="ResultsItem-flip-card-back-image-logo"
                 src={nationality === "Fire Nation" ? `img/Logo/logo-${nationality}.png` : nationality === "Air Nomad" ? `img/Logo/logo-${nationality}.png` : nationality === "Earth Kingdom" ? `img/Logo/logo-${nationality}.png` : `img/Logo/logo-${ethnicity}.png`}
-                alt={`Nation logo`}
+                alt="Nation logo"
               />
             </div>
             <aside className="ResultsItem-flip-card-back-info">
               <hr className="ResultsItem-flip-card-back-info-breakline" />
               <h3 className="ResultsItem-flip-card-back-info-title-first">First apparence:</h3>
-              <p className="ResultsItem-flip-card-back-info-firstAppearance">{firstAppearance.length === 46 ? firstAppearance.slice(0, 20) : firstAppearance.length < 26 ? firstAppearance : firstAppearance.slice(0, 51)}</p>
+              <p className="ResultsItem-flip-card-back-info-firstAppearance">{firstAppearance?.length === 46 ? firstAppearance?.slice(0, 20) : firstAppearance?.length < 26 ? firstAppearance : firstAppearance?.slice(0, 51)}</p>
               <h3 className="ResultsItem-flip-card-back-info-title">Last apparence:</h3>
-              <p className="ResultsItem-flip-card-back-info-lastAppearance">{lastAppearance === "NA" ? "Unknown" : lastAppearance[0]}</p>
+              <p className="ResultsItem-flip-card-back-info-lastAppearance">{lastAppearance === "NA" ? "Unknown" : Array.isArray(lastAppearance) ? lastAppearance[0] : lastAppearance}</p>
               <h3 className="ResultsItem-flip-card-back-info-title">Voiced by:</h3>
-              <p className="ResultsItem-flip-card-back-info-voicedBy">{voicedBy === "NA" ? "Unknown" : voicedBy[0]}</p>
+              <p className="ResultsItem-flip-card-back-info-voicedBy">{voicedBy === "NA" ? "Unknown" : Array.isArray(voicedBy) ? voicedBy[0] : voicedBy}</p>
             </aside>
             <Link to={id === 8 ? `/character/8/Suki` : id === 11 ? `/character/11/Azula` : `/character/${id}/${name}`} className="ResultsItem-flip-card-back-link">
               <FaPlusCircle className="ResultsItem-flip-card-back-link-plus" size="1.5rem" />
